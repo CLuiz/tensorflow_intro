65015f3a75f7ddd3102f83520f6fb5095d88e856
Chris Luiz <chris@chrisluiz.io>
2017-04-20 05:47:53 -0600

    activation function overview

--- activation_functions.py
+++ activation_functions.py
@@ -18,3 +18,43 @@ def plot_act(i=1.0, actfunc=lambda x: x):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1)
+
+# Start a session
+sess = tf.Session()
+# Create a simple input of 3 real values
+i = tf.constant([1.0, 2.0, 3.0], shape=[1, 3])
+# Create a matrix of weights
+w = tf.random_normal(shape=[3, 3])
+# Create a vector of biases
+b = tf.random_normal(shape=[1, 3])
+# Dummy activation function
+def func(x): return x
+# tf.matmul will multiply the input(i) tensor and the weight(w) tensor then
+# sum the result with the bias(b) tensor.
+act = func(tf.matmul(i, w) + b)
+# Evaluate the tensor to a numpy array
+act.eval(session=sess)
+
+# Plot step function
+plot_act(1.0, func)
+
+# Plot sigmoid activation function
+plot_act(1, tf.sigmoid)
+
+# Use of sigmoid in neural net layer
+act = tf.sigmoid(tf.matmul(i, w) + b)
+act.eval(session=sess)
+
+# Plot hyperbolic tangent
+plot_act(1, tf.tanh)
+
+# Use tanh in a neural net layer
+act = tf.tanh(tf.amtmul(i, w) + b)
+act.eval(session=sess)
+
+# Parametric rectified linear units (ReLU)
+plot_act(1, tf.nn.relu)
+
+# Using ReLU in a neural net layer
+act = tf.nn.relu(tf.matmul(i, w) + b)
+act.eval(session=sess)
