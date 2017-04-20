9e9a302fe8acf51609117d9c39176d2a32e6188d
Chris Luiz <chris@chrisluiz.io>
2017-04-20 06:26:07 -0600

    simple operations

---
+++ tf_hello_world_lab.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+a = tf.constant([5])
+b = tf.constant([2])
+
+# Simple addition
+c = tf.add(a, b)
+
+with tf.Session() as sess:
+    result = sess.run(c)
+    print('The addition of these two constants {0} and {1} is: {2}'.format(a,b,result))
+
+# Multiplication
+c = a * b
+
+with tf.Session() as sess:
+    result = sess.run(c)
+    print('The multiplication of these two constants is: {0}'.format(result))
+
+# Elementwise & Matrix Multiplication
+matrixA = tf.constant([[2,3], [3,4]])
+matrixB = tf.constant([[2,3],[3,4]])
+
+elementwise_mult_result = tf.multiply(matrixA, matrixB)
+matrix_mult_result = tf.matmul(matrixA, matrixB)
+
+with tf.Session() as sess:
+    result1 = tf.multiply(matrixA, matrixB)
+    print(f'The result of elementwise multiplication of these two matrices is: {result1}')
+    
+    result2 = tf.matmul(matrixA, matrixB)
+    print(f'The result of multiplying these two matrices is: {result2}')
