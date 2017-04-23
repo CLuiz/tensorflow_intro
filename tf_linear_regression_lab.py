bf887539bba613c4faeb27ae4afbe74deec1cb0e
Chris Luiz <chris@chrisluiz.io>
2017-04-22 21:41:24 -0600

    linear regression finished

--- tf_linear_regression_lab.py
+++ tf_linear_regression_lab.py
@@ -10,12 +10,12 @@ df = pd.read_csv(
 # Set chirps and temp as our x and y values
 x_data, y_data = (df['Chirps'].values, df['Temp'].values)
 
-# Initialize plot to examine relationship
-plt.plot(x_data, y_data, 'ro')
-
-# Set axes labels
-plt.xlabel('# Chirps per 15 sec')
-plt.ylabel('Temp in Farenheit')
+# # Initialize plot to examine relationship
+# plt.plot(x_data, y_data, 'ro')
+#
+# # Set axes labels
+# plt.xlabel('# Chirps per 15 sec')
+# plt.ylabel('Temp in Farenheit')
 
 # plt.show()
 
@@ -32,16 +32,63 @@ c = tf.Variable(2.0)
 Y_pred = tf.add(tf.multiply(X, m), c)
 
 # Create session and initialize variables
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    pred = sess.run(Y_pred, feed_dict={X: x_data})
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+pred = sess.run(Y_pred, feed_dict={X: x_data})
 
-# Plot initial prediction against datapoints
-plt.plot(x_data, pred)
-plt.plot(x_data, y_data, 'ro')
-
-# Set axes labels
-plt.xlabel('# Chirps per 15 sec')
-plt.ylabel('Temp in Farenheit')
+# # Plot initial prediction against datapoints
+# plt.plot(x_data, pred)
+# plt.plot(x_data, y_data, 'ro')
+#
+# # Set axes labels
+# plt.xlabel('# Chirps per 15 sec')
+# plt.ylabel('Temp in Farenheit')
 
 # plt.show()
+
+# Define a graph for the loss function
+
+# Normalization factor
+nf = 1e-1
+
+# Set up loss function
+loss = tf.reduce_mean(tf.squared_difference(Y_pred*nf, Y*nf))
+
+# Define an optimization graph w/ gradient descent & adagrad
+optimizer1 = tf.train.GradientDescentOptimizer(learning_rate=.01)
+optimizer2 = tf.train.AdagradOptimizer(0.01)
+
+# Pass the loss function to the optimizer
+train = optimizer1.minimize(loss)
+
+# Initialize variables again
+sess.run(tf.global_variables_initializer())
+
+convergenceTolerance = 0.0001
+previous_m = np.inf
+previous_c = np.inf
+
+steps = {}
+steps['m'] = []
+steps['c'] = []
+
+losses=[]
+
+for k in range(100000):
+    _, _m, _c, _l = sess.run([train, m, c, loss],
+                            feed_dict={X: x_data, Y: y_data})
+    steps['m'].append(_m)
+    steps['c'].append(_c)
+    losses.append(_l)
+    if (np.abs(previous_m - _m) or np.abs(previous_c - _c) ) <= convergenceTolerance :
+
+        print("Finished by Convergence Criterion")
+        print(k)
+        print(_l)
+        break
+    previous_m = _m,
+    previous_c = _c,
+
+plt.plot(losses[:])
+plt.show()
+sess.close()
