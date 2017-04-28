9b07d4aa7cbb66cfd58071685ab5d5439887d245
Chris Luiz <chris@chrisluiz.io>
2017-04-28 10:20:58 -0600

    rnn on mnist done

--- tf_rnn_lstm_mnist.py
+++ tf_rnn_lstm_mnist.py
@@ -84,3 +84,84 @@ y = tf.placeholder(dtype='float', shape=[None, n_classes], name='y')
 
 weights = {'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))}
 biases = {'out': tf.Variable(tf.random_normal([n_classes]))}
+
+# Define lstm cell with tensorflow
+lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden,
+                                         forget_bias=1.0,
+                                         state_is_tuple=True)
+# initial_state
+# initial_state = (tf.zeros([1, n_hidden]),) * 2
+
+
+# Define rnn function
+def RNN(x, weights, biases):
+
+    # Prepare data shape to match rnn function requirements
+    # Current data input shape: (batch_size, n_steps, n_input)
+    # [100 x 28 x 28]
+
+    # Define cell
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden,
+                                             forget_bias=1.0)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell,
+                                        inputs=x,
+                                        dtype=tf.float32)
+    # The output of the rnn would be a [100 x 28 x 128] matrix.
+    # Use linear activation to map it to a [? x 10] matrix.
+    output = tf.reshape(tf.split(outputs,
+                                 28,
+                                 axis=1,
+                                 num=None,
+                                 name='split')[-1], [-1, 128])
+    return tf.matmul(output, weights['out']) + biases['out']
+
+
+with tf.variable_scope('forward3'):
+    pred = RNN(x, weights, biases)
+
+# Define cost function and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,
+                                                              logits=pred))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Calculate model accuracy
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    step = 1
+    # Keep training until reaching max iterations
+    while step * batch_size < training_iters:
+        # Read batch of 100 images as 1 sequence
+        # batch_y is a matrix of [100 x 10]
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+
+        # Consider each row of the image as one sequence
+        # Reshape data to get 28 seq of 28 elements so that
+        # batxh_x is [100 x 28 x 28]
+        batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+
+        # Run optimizer op (backprop)
+        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
+
+        if step % display_step == 0:
+            # Calculate batch accuracy
+            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+            # Calculate batch loss
+            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+            print(f'Iter {step * batch_size}')
+            print(f'Minibatch loss: {loss}')
+            print(f'Training accuracy: {acc}')
+        step += 1
+    print('Optimization finished!')
+
+    # Calcualte accuracy for 128 mnist test images
+    test_len = 128
+    test_data = mnist.test.images[:test_len].reshape((-1, n_steps, n_input))
+    test_label = mnist.test.labels[:test_len]
+    print(f'Testing Accuracy: {sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})}')
+
+sess.close()
