c2072a36c0c8510c2e5c2392a951c876e1643e9d
Chris Luiz <chris@chrisluiz.io>
2017-04-30 05:57:08 -0600

    rbm collaborative filtering work

--- tf_RBM_collaborative_filtering.py
+++ tf_RBM_collaborative_filtering.py
@@ -34,7 +34,7 @@ for user_id, cur_user in user_group:
     # Create temp that stores every movie's rating
     temp = [0] * len(movies_df)
     # For each movie im cur_user's movie list
-    for num, movie in cur_user.itterows():
+    for num, movie in cur_user.iterrows():
         # Divide the ratings by 5 and store
         temp[movie['list_index']] = movie['rating'] / 5.0
     # Add the list of ratings into the training list
@@ -76,3 +76,59 @@ CD = (w_pos_grad - w_neg_grad) / tf.to_float(tf.shape(v0)[0])
 update_w = W + alpha * CD
 update_vb = vb + alpha * tf.reduce_mean(v0 - v1, 0)
 update_hb = hb + alpha * tf.reduce_mean(h0 - h1, 0)
+
+# Set error function
+err = v0 - v1
+err_sum = tf.reduce_mean(err * err)
+
+# --- Initialize variables ---
+# Current Weight
+cur_w = np.zeros([visible_units, hidden_units], np.float32)
+# Crrent visible unit biases
+cur_vb = np.zeros([visible_units], np.float32)
+# Current hidden unit biases
+cur_hb = np.zeros([hidden_units], np.float32)
+# Previous weights
+prv_w = np.zeros([visible_units, hidden_units], np.float32)
+# Previous visible unit biases
+prv_vb = np.zeros([visible_units], np.float32)
+# Previous hidden unit biases
+prv_hb = np.zeros([hidden_units], np.float32)
+# Set session and run init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# --- Training loop ---
+
+epochs = 15
+batchsize = 100
+errors = []
+
+for i in range(epochs):
+    for start, end in zip(range(0, len(train_X), batchsize),
+                          batchsize, len(train_X), batchsize):
+        # define batch and feed_dict for update sequence
+        batch = train_X[start:end]
+        fdict = {v0: batch,
+                 W: prv_w,
+                 vb: prv_vb,
+                 hb: prv_hb}
+        # Calculate current values and update weighs & biases
+        cur_w = sess.run(update_w, fdict)
+        cur_vb = sess.run(update_vb, fdict)
+        cur_hb = sess.run(update_hb, fdict)
+        prv_w = cur_w
+        prv_vb = cur_vb
+        prv_hb = cur_hb
+
+    # Calculate and record errors for this loop
+    errors.append(sess.run(err_sum, feed_dict={v0: train_X,
+                                               W: cur_w,
+                                               vb: cur_vb,
+                                               hb: cur_hb}))
+# Plot errors by epoch
+plt.plot(errors)
+plt.ylabel('Error')
+plt.xlabel('Epoch')
+plt.show()
