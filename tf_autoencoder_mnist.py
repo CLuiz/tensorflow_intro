744fa89f3093e646837f59240912d565e6fff2c4
Chris Luiz <chris@chrisluiz.io>
2017-05-01 08:16:39 -0400

    autoencoder almost done

--- tf_autoencoder_mnist.py
+++ tf_autoencoder_mnist.py
@@ -4,8 +4,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorails.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 
+# Read in data
+mnist = input_data.read_data_sets('tmp/data/', one_hot=True)
 # -- Set parameters ---
 learning_rate = 0.01
 epochs = 20
@@ -24,7 +26,7 @@ n_input = 784
 X = tf.placeholder('float', [None, n_input])
 
 weights = {
-    'encoder_h1': tf.Variable(tf.random_normal([n_input. n_hidden_1])),
+    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
     'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
     'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
     'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input]))
@@ -59,4 +61,50 @@ def decoder(x):
                                    biases['decoder_b1']))
     return layer_2
 
-# Construct madel
+
+# Construct model
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# Prediction
+y_pred = decoder_op
+y_true = X
+
+# Define loss and optimizer, minimize squared error
+cost = tf.treduce_mean(tf.pow(y_true - y_pred, 2))
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+# Intializing variables
+init = tf.global_variables_initializer()
+
+# Launch graph and initialize interactive session
+sess = tf.InteractiveSession()
+sess.run(init)
+
+total_batch = int(mnist.train.num_examples / batch_size)
+
+# Training cycle
+for epoch in range(epochs):
+    # Loop over each batch
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        # Run optimization operation and cost function
+        _, c = sess.run([optimizer, cost], feed_dict={batch_xs})
+        # Display logs per epoch step
+    if epoch % display_step == 0:
+        print('*' * 30)
+        print('Epoch: {epoch +1}')
+        print('Cost: {c}')
+        print('*' * 30)
+print('*' * 30)
+print('Optimization Finished')
+print('*' * 30)
+
+# Apply encode and decode over the  test set
+encode_decode = sess.run(y_pred,
+                         feed_dict={X: mnist.test.images[:examples_to_show]})
+# Compare original images with their reconstructions
+f, a = plt.subplots(2, 10, figsize=(10, 2))
+for i in range(examples_to_show):
+    a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+    a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
