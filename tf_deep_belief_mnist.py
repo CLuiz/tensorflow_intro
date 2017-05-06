cef3daadfd2a108f24c13ea2e00a0844fff1b2b8
Chris Luiz <chris@chrisluiz.io>
2017-05-06 09:35:26 -0400

    deep belief built & ready for debugging

--- tf_deep_belief_mnist.py
+++ tf_deep_belief_mnist.py
@@ -117,6 +117,83 @@ class NN(object):
 
         # Initialization loop
         for size in self.sizes + [Y.shape[1]]:
+            # Define upper limit for the uniform distribution range
+            max_range = 4 * math.sqrt(6. / (input_size + size))
+
+            # Initialize weights thrugh a random uniform distribution
+            self.w_list.append(np.random.uniform(-max_range,
+                               max_range,
+                               [input_size, size]).astype(np.float32))
+
+            # Intitalize bias as zeros
+            self.b_list.append(np.zeros([size], np.float32))
+            input_size = size
+
+    def load_from_rbms(self, dbn_sizes, rbm_list):
+        # Check if expected sizes are correct
+        assert len(dbn_sizes) == len(self._sizes)
+
+        for i in range(len(self._sizes)):
+            # Check if for each RBN the expected sizes are correct
+            assert dbn_sizes[i] == self._sizes[i]
+
+        # If everything is correct, bring over the weights and biases
+        for i in range(len(self._sizes)):
+            self.w_list[i] = rbm_list[i].w
+            self.b_list[i] = rbm_list[i].hb
+
+    def train(self):
+        # Create placeholders for input, weights, biases, output
+        _a = [None] * (len(self._sizes) + 2)
+        _w = [None] * (len(self._sizes) + 1)
+        _b = [None] * (len(self._sizes) + 1)
+        _a[0] = tf.placeholder('float', [None, self._X.shape[1]])
+        y = tf.placeholder('float', [None, self._Y.shape[1]])
+
+        # Define variables and actiavation function
+        for i in range(len(self._sizes) + 1):
+            _w[i] = tf.Variable(self.w_list[i])
+            _b[i] = tf.Variable(self.b_list[i])
+        for i in range(1, len(self._sizes) + 2):
+            _a[i] = tf.nn.sigmoid(tf.matmul(_a[i - 1], _w[i - 1]) + _b[i - 1])
+
+        # Define the cost function
+        cost = tf.reduce_mean(tf.square(_a[-1] - y))
+
+        # Define the training operation
+        train_op = tf.train.MomentumOptimizer(self._learning_rate, self._momentum).minimize(cost)
+
+        # Prediction operation
+        predict_op = tf.argmax(_a[-1], 1)
+
+        # Training Loop
+        with tf.Session() as sess:
+            # Initailize Variables
+            sess.run(tf.global_variables_initializer())
+            # For each epoch
+            for i in range(self._epochs):
+                # For each step
+                for start, end in zip(range(0,
+                                            len(self.X),
+                                            self._batchsize),
+                                      range(self._batchsize,
+                                            len(self._X),
+                                            self._batchsize)):
+                    # Run the training operation on the input data
+                    sess.run(train_op, feed_dict={_a[0]: self._X[start:end],
+                                                  y: self._Y[start:end]})
+                for j in range(len(self._sizes) + 1):
+                    # Retrieve weights and biases
+                    self.w_list[j] = sess.run(_w[j])
+                    self.b_list[j] = sess.run(_b[j])
+
+                print("Accuracy rating for epoch " + str(i) + ": " + str(np.mean(np.argmax(self._Y,
+                                        axis=1) ==
+                                        sess.run(predict_op,
+                                                 feed_dict={_a[0]: self._X,
+                                                            y: self._Y}))))
+
+                # print(f'Accuracy rating for epoch {str(i)}: ')
 
 
 if __name__ == '__main__':
